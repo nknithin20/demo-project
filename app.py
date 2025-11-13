6379386a29b4851f3913224fe281cc83d8fa0dd7
Nithin <nknithin20@gmail.com>
2025-11-13 09:51:46 +0530

    change variable form a to num

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-a = int(input("enter a number"))
-if a % 2 == 0:
+num = int(input("enter a number"))
+if num % 2 == 0:
     print("whether is to cool")
 else:
     ptint("whether is to hot")
-else:
-    print("Python is programming language")
+
